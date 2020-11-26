102f4b94cc3087ca5fc245664d3603c734dbe84f
adhyuva <adhyuva8@gmail.com>
2020-11-26 18:13:57 -0500

    Fixing time.

--- js/time.js
+++ js/time.js
@@ -13,4 +13,7 @@
     function checkTime(i) {
           if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
           return i;
+          if(h>12){
+          h = h - 12;
+          }
     }
