12cfc35d1ec361537038a0c037bb5dc1c70872db
adhyuva <adhyuva8@gmail.com>
2020-11-27 18:24:50 -0500

    from military to regular time. Check time.js

--- js/time.js
+++ js/time.js
@@ -3,17 +3,27 @@
           var h = today.getHours();
           var m = today.getMinutes();
           var s = today.getSeconds();
+          h = checkTime(h);
+          h = regTime(h);
           m = checkTime(m);
           s = checkTime(s);
           document.getElementById('txt').innerHTML =
           h + ":" + m + ":" + s;
-          var t = setTimeout(startTime, 500);
+          var t = setTimeout(startTime, 100);
+    }
+    function regTime(i){
+        if (i > 12){
+        i = i - 12;
+        }
+
+    return i;
     }
 
+
     function checkTime(i) {
-          if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-          return i;
-          if(h>12){
-          h = h - 12;
-          };
+          if (i < 10) {
+          i = "0" + i;
+                       }
+    return i;
+
     }
