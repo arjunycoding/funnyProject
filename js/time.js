67c704e41ac1da4106be92774ec35a81eb54f839
adhyuva <adhyuva8@gmail.com>
2020-12-05 12:17:48 -0500

    Changing back tto normal-time.js

--- js/time.js
+++ js/time.js
@@ -1,29 +1,29 @@
-       function startTime() {
-              var today = new Date();
-              var h = today.getHours();
-              var m = today.getMinutes();
-              var s = today.getSeconds();
-              h = checkTime(h);
-              h = regTime(h);
-              m = checkTime(m);
-              s = checkTime(s);
-              document.getElementById('txt').innerHTML =
-              h + ":" + m + ":" + s;
-              var t = setTimeout(startTime, 100);
-        }
-        function regTime(i){
-            if (i > 12){
-            i = i - 12;
-            }
+        function startTime() {
+                     var today = new Date();
+                     var h = today.getHours();
+                     var m = today.getMinutes();
+                     var s = today.getSeconds();
+                     h = checkTime(h);
+                     h = regTime(h);
+                     m = checkTime(m);
+                     s = checkTime(s);
+                     document.getElementById('txt').innerHTML =
+                     h + ":" + m + ":" + s;
+                     var t = setTimeout(startTime, 100);
+               }
+               function regTime(i){
+                   if (i > 12){
+                   i = i - 12;
+                   }
 
-        return i;
-        }
+               return i;
+               }
 
 
-        function checkTime(i) {
-              if (i < 10) {
-              i = "0" + i;
-                           }
-        return i;
+               function checkTime(i) {
+                     if (i < 10) {
+                     i = "0" + i;
+                                  }
+               return i;
 
-        }
+               }
