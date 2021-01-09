77fdcaeba98e7dad2c893b65dcd343ba897593dd
adhyuva <adhyuva8@gmail.com>
2021-01-09 12:48:02 -0500

    Making real tabs!

--- Awesome/awesometabs.js
+++ Awesome/awesometabs.js
@@ -1,13 +1,13 @@
- function eraser(evt, divContent) {
-        var i, tabcontent, topb;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-        tablinks = document.getElementsByClassName("topb");
-        for (i = 0; i < topb.length; i++) {
-            topb[i].className = topb[i].className.replace(" active", "");
-        }
-        document.getElementById(divContent).style.display = "block";
-        evt.currentTarget.className += " active";
-    }
+function Awesome(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
