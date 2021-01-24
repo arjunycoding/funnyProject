15200555cc4e0a2808826e3612a56b3d29c5c4f8
arjunyuvaraj <arjunycoding@gmail.com>
2021-01-24 13:13:53 -0500

    making the game.

--- Arjun/penguin/penguin.js
+++ Arjun/penguin/penguin.js
@@ -1,63 +1,60 @@
-/*
-       function welcome() {
-           var today = new Date();
-           var hourNow = today.getHours();
-           var greeting;
-
-           if (hourNow > 18) {
-               greeting = 'Good evening!';
-           } else if (hourNow > 12) {
-               greeting = 'Good afternoon!';
-           } else if (hourNow > 0) {
-               greeting = 'Good morning!';
-           } else {
-               greeting = 'Welcome!';
-           }
-
-           document.write('<h3>' + greeting + '</h3>');
-       }
-
-       function example {
-           let sale = true;
-
-           sale = false;
-
-           if(sale) {
-             document.write('Time to buy!');
-           } else {
-             document.write('Time to wait for a sale.')
-           }
-       }
-       function myFunction() {
-         document.getElementById("demo").innerHTML = example();
-
-       }
-*/
 var i = 1;       // Set counter to 1
-var msg1 = '';    // Message
-var msg2 = '';
-var msg3 = '';
-var msg4 = '';
-var msg5 = '';
+var msg = '';  // Message
+var number = {
+  one: 1,
+  two : 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  ten: 10,
+  eleven: 11,
+  twelve: 12
+  }
+    var msgOne = '';
+    var msgTwo = '';
+    var msgThree = '';
+    var msgFour = '';
+    var msgFive = '';
+    var msgSix = '';
+    var msgSeven = '';
+    var msgEight = '';
+    var msgNine = '';
+    var msgTen = '';
+    var msgEleven = '';
+    var msgTwelve = '';
 
 // Store 5 times table in a variable
 while (i < 13) {
-  msg1 += i + ' x 1 = ' + (i * 1) + '<br />';
+  msgOne += i + ' x ' + number.one + ' = ' + (i * number.one) + '<br />';
   i++;
 }
 while (i < 13) {
-  msg2 += i + ' x 2 = ' + (i * 2) + '<br />';
+  msgTwo += i + ' x ' + number.two + ' = ' + (i * number.two) + '<br />';
   i++;
 }
 while (i < 13) {
-  msg3 += i + ' x 3 = ' + (i * 3) + '<br />';
+  msgThree += i + ' x ' + number.three + ' = ' + (i * number.three) + '<br />';
   i++;
 }
 while (i < 13) {
-  msg4 += i + ' x 4 = ' + (i * 4) + '<br />';
+  msgFour += i + ' x ' + number.four + ' = ' + (i * number.four) + '<br />';
   i++;
 }
 
-document.getElementById('answer1').innerHTML = msg1;
-document.getElementById('answer2').innerHTML = msg2;
-document.getElementById('answer3').innerHTML = msg3;
+document.getElementById('board1').innerHTML = msgOne;
+document.getElementById('board2').innerHTML = msgTwo;
+document.getElementById('board3').innerHTML = msgThree;
+document.getElementById('board4').innerHTML = msgFour;
+document.getElementById('board5').innerHTML = msgFive;
+document.getElementById('board6').innerHTML = msgSix;
+document.getElementById('board7').innerHTML = msgSeven;
+document.getElementById('board8').innerHTML = msgEight;
+document.getElementById('board9').innerHTML = msgNine;
+document.getElementById('board10').innerHTML = msgTen;
+document.getElementById('board11').innerHTML = msgEleven;
+document.getElementById('board12').innerHTML = msgTwelve;
+
